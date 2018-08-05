8d4486e947a0a5f267b93857d8609d8202df3fee
zaurdk <zaurdk@gmail.com>
2018-08-05 15:48:02 +0500

    First commit

---
+++ name.js
@@ -0,0 +1,19 @@
+﻿var x = 5;
+var y;
+var result;
+y = 5;
+result = x + y;
+document.write(x + ' + ' + y + ' = ' + (x+y));
+var str_1 = '12';
+var str_2 = '24';
+document.write('<br />');
+document.write(str_1 + str_2);
+document.write('<br />');
+var bool;
+var z = 7;
+var d = 7;
+
+bool = z >= d
+document.write(bool);
+document.write('<br />');
+document.write(10 + ' % ' + 7 + ' = ' + (10%7));
