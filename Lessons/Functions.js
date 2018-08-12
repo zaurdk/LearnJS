ef1c3cc11bfd2fdb39614072c9f93fbbbfd52443
zaurdk <zaurdk@gmail.com>
2018-08-12 13:20:26 +0500

    Homework lessons

---
+++ Lessons/Functions.js
@@ -0,0 +1,8 @@
+﻿function checkAge(age) {
+    return(age > 18) ? true: confirm('Родители разрешили?');
+  }
+
+
+function checkAge(age) {
+    return(age > 18) || confirm('Родители разрешили?');
+  }
---
+++ Lessons/Functions1.js
@@ -0,0 +1,7 @@
+function min(a, b) {
+  if (a > b) {
+      return b;
+  }  else {
+      return a;
+  };
+};
