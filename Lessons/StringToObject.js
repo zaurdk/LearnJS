b7569bc6f68ee15765a35bbdfac064c1381e0f12
zaurdk <zaurdk@gmail.com>
2018-08-15 22:15:54 +0500

    Next try

---
+++ Lessons/StringToObject.js
@@ -0,0 +1,50 @@
+let style = `
+  position: absolute;
+  top: -999px;
+  left: 0px;
+  right: auto;
+  bottom: auto;
+  border: 0px;
+  box-sizing: content-box;
+  word-wrap: break-word;
+  overflow: hidden;
+  height: 0px !important;
+  min-height: 0px !important;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 12px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: 0px;
+  text-transform: none;
+  word-spacing: 0px;
+  text-indent: 0px;
+  line-height: 20px;
+  width: 191px;
+`;
+
+function styleToObject(str) {
+    var pos = 0;
+    var obj = {};
+
+    while (true) {
+         var keyWordStart = str.indexOf('\n', pos);
+         var keyWordStop = str.indexOf(':', keyWordStart);
+         var valueStart = keyWordStop + 1;
+         var valueStop = str.indexOf(';', valueStart);
+
+        if (keyWordStart == -1) break;
+
+        var keyWord = str.slice(keyWordStart, keyWordStop);
+        var value = str.slice(valueStart, valueStop);
+    
+        obj[keyWord] = value;
+    
+        pos = pos + valueStop; 
+
+    }
+
+    return obj;  
+ 
+}
+
+console.log(styleToObject(style));
