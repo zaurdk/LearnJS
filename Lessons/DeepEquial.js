aa0f332fe353f9119c5f979c5bb7d3ae576cb1a1
zaurdk <zaurdk@gmail.com>
2018-08-15 21:01:06 +0500

    Next try

--- Lessons/DeepEquial.js
+++ Lessons/DeepEquial.js
@@ -16,8 +16,34 @@ let b = {
   age: 10
 };
 
+function keysScore(obj1, obj2) {
+    var count = 0;
+    var count1 = 0;
+
+    for (var key in obj1) if (a.hasOwnProperty(key)) count++;
+    for (var key1 in obj2) if (a.hasOwnProperty(key1)) count1++;
+
+    return (count === count1);
+};
 
 
-console.log(objectString);
-console.log(objectString1);
+function deepEqual(obj1, obj2) {
+    if (keysScore(obj1, obj2) === false) {
+        return false;
+    } else {    
+        for (var propName in obj1, obj2) {
+            if (typeof (obj1[propName]) === 'object') {
+                let result = (deepEqual(obj1[propName], obj2[propName]));
+                return result;
+             } else {
+                (obj1[propName] === obj2[propName]) ? true : false;
+             }
+        }
+    }
+}
+
+
+
+console.log(keysScore(a, b));
+
 console.log(deepEqual(a, b));
