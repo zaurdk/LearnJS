228434ba9cf329b0921e6dec86186f6018445468
zaurdk <zaurdk@gmail.com>
2018-08-18 21:23:31 +0500

    Next practice

---
+++ Additional Tasks/GetRanged.js
@@ -0,0 +1,40 @@
+//function getRange(1, 2, 3)
+//1 аргумент – количество элементов
+//2 аргумент – значение первого элемента
+//3 аргумент – шаг 
+
+//console.log(getRange(3)); // [1, 2, 3] — первый элемент по умолчанию 1, шаг по умолчанию 1
+//console.log(getRange(2, 2)) // [2, 3]
+//console.log(getRange(3, 2, 2)) // [2, 4, 6]
+
+
+
+function getRange(elementScore, firstElementValue, elementStep) {
+
+    var arr = [];
+
+    if (firstElementValue === undefined && elementStep === undefined) {
+        for (i = 1; i <= elementScore; i++) {
+            arr.push(i); 
+        }
+    } else if (elementStep == undefined) {
+        for (i = 1; i <= elementScore; i++) {
+            arr.push(firstElementValue);
+            firstElementValue++
+        }
+    } else {
+        for (i = 1; i <= elementScore; i++) {
+            arr.push(firstElementValue);
+            firstElementValue += elementStep; 
+        }
+    }
+
+    return arr;
+}
+
+
+
+console.log(getRange(3)); // [1, 2, 3]
+console.log(getRange(2, 2)) // [2, 3]
+console.log(getRange(3, 2, 2)) // [2, 4, 6]
+
---
+++ Additional Tasks/MAPthrowReduse.js
@@ -0,0 +1,21 @@
+var arr = [];
+
+function callback (arrElement) {
+    return arrElement > 0;
+};
+
+
+function filter(array, callback) {
+    var filteredArr = [];
+    
+    filteredArr = array.reduce(function(value) {
+        return ;
+      }, 0);
+      
+    
+
+    return filteredArr;
+}
+
+
+console.log(filter([1, -1, 2, -2, 3], callback));
